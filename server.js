fa8d41902c07ac26d2fa9c3e822772fc2bd27fe5
DavideMarietti <76477890+DavideMarietti@users.noreply.github.com>
2023-02-02 12:34:13 +0100

    Aggiornato e funzionante all'esercizio 5

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
 const express = require('express');
-
+const session = require('express-session');
 const userfile = require('./js/user-file');
+
+const {createApp} = require("./js/load-data");
+
+const appLogic = createApp();
 
 const port = 3000;
 const app = express();
 const router = express.Router();
 
-const {createApp} = require("./js/load-data");
-
-const appLogic = createApp();
+app.use(session({
+    secret: "pincopallinoecappuccettorosso",
+    resave: false,
+    saveUninitialized: false
+}))
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 router.route("/user")
     .get((req, res) => {
+        console.log(req.session)
+
         if (req.query.id !== undefined && userfile.userExists(req.query.id)) {
             userfile.sendFile(req.query.id, res);
         } else {
@@ -26,14 +34,42 @@ router.route("/user")
         res.sendStatus(200);
     });
 
-router.route("/:username")
+router.route("/login/:username")
     .get((req, res) => {
         const name = req.params.username;
-        const isuser = (name.length > 0 && appLogic.users.hasOwnProperty(name));
+        const isuser = (name.length > 0
+            && appLogic.users.hasOwnProperty(name));
         if (isuser) {
-            res.send(appLogic.getUserPlants(name));
+            req.session.username = name;
+            res.send("<h3>Login successful</h3>");
         } else {
             res.sendStatus(404);
+        }
+    });
+
+router.route("/logout")
+    .get((req, res) => {
+        if (req.session.username !== undefined) {
+            req.session.destroy();
+            res.send("<h3>Logout successful</h3>");
+        } else {
+            res.send("<h3>No logged users</h3>");
+        }
+    });
+
+router.route("/collection")
+    .get((req, res) => {
+        console.log(req.session);
+        if (req.session.username !== undefined) {
+            const name = req.session.username;
+            const isuser = (name.length > 0 && appLogic.users.hasOwnProperty(name));
+            if (isuser) {
+                res.send(appLogic.getUserPlants(name));
+            } else {
+                res.sendStatus(404);
+            }
+        } else {
+            res.send("<h3>No logged users</h3>");
         }
     });
 
