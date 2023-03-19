526caa4f4c435f08e2e9a7804748d60989f3c871
Lorenz3iGianzo <119524771+Lorenz3iGianzo@users.noreply.github.com>
2023-03-19 17:59:00 +0100

    Moved tha nav bar and added brand logo

--- forum-proj/src/app/core/core.module.ts
+++ forum-proj/src/app/core/core.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { LeftNavComponent } from './left-nav/left-nav.component';
+import { RightNavComponent } from './right-nav/right-nav.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    JumbotronComponent
+    JumbotronComponent,
+    LeftNavComponent,
+    RightNavComponent
   ],
   imports: [
     CommonModule
--- forum-proj/src/app/core/jumbotron/jumbotron.component.ts
+++ forum-proj/src/app/core/jumbotron/jumbotron.component.ts
@@ -10,6 +10,7 @@ export class JumbotronComponent implements OnInit {
   @Input() titolo: string = "";
   @Input() sottotitolo: string = "";
   @Input() Show: boolean = true;
+  @Input() autenticato: boolean | null = false;
 
   constructor() { }
 
