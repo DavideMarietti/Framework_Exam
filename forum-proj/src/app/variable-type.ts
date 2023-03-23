81741aa2686827212102b431c8a4dac6186d7b75
Lorenz3iGianzo <119524771+Lorenz3iGianzo@users.noreply.github.com>
2023-03-23 15:07:27 +0100

    added users json

--- forum-proj/src/app/variable-type.ts
+++ forum-proj/src/app/variable-type.ts
@@ -1,11 +1,11 @@
 export class Utente {
-  public username : string;
-  public password: string;
-  public nome : string;
-  public cognome : string;
-  public sesso : string;
-  public eta : number;
-  public image: string;
+  username : string;
+  password: string;
+  nome : string;
+  cognome : string;
+  sesso : string;
+  eta : number;
+  image: string;
   constructor(username: string, password: string, nome: string, cognome: string, sesso: string, eta: number, image: string) {
     this.username = username;
     this.password = password;
