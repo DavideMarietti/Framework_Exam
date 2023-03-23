16cfa8931f6d1e177286f28393812201a54bcd51
DavideMarietti <76477890+DavideMarietti@users.noreply.github.com>
2023-03-23 12:54:29 +0100

    Checkpoit two services working (user post).

--- backendApp/post-service/src/main/java/com/javasampleapproach/springrest/postgresql/repo/PostRepository.java
+++ backendApp/post-service/src/main/java/com/javasampleapproach/springrest/postgresql/repo/PostRepository.java
@@ -1,10 +1,12 @@
 package com.javasampleapproach.springrest.postgresql.repo;
 
 import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
+
 import com.javasampleapproach.springrest.postgresql.model.Post;
 
 public interface PostRepository extends CrudRepository<Post, Long> {
-
+	List<Post> findByAge(int age);
 }
 
