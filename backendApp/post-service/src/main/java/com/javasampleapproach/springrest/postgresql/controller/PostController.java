e53fc7ef0fd09374ac7ca3e3bc7f3a528fa1c341
DavideMarietti <76477890+DavideMarietti@users.noreply.github.com>
2023-03-28 19:32:19 +0200

    added service folder to post obj.

--- backendApp/post-service/src/main/java/com/javasampleapproach/springrest/postgresql/controller/PostController.java
+++ backendApp/post-service/src/main/java/com/javasampleapproach/springrest/postgresql/controller/PostController.java
@@ -1,11 +1,8 @@
 package com.javasampleapproach.springrest.postgresql.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,73 +15,47 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.javasampleapproach.springrest.postgresql.model.Post;
-import com.javasampleapproach.springrest.postgresql.repo.PostRepository;
+import com.javasampleapproach.springrest.postgresql.service.PostService;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1")
 public class PostController {
 
+	private final PostService postService;
+
 	@Autowired
-	PostRepository repository;
+	public PostController(PostService postService) {
+		this.postService = postService;
+	}
 
 	@GetMapping("/posts")
 	public List<Post> getAllPosts() {
-		System.out.println("Get all Posts...");
-
-		List<Post> posts = new ArrayList<>();
-		repository.findAll().forEach(posts::add);
-
-		return posts;
+		return postService.getAllPosts();
 	}
 
 	@PostMapping(value = "/posts/create")
 	public Post postPost(@RequestBody Post post) {
-
-		Post _post = repository.save(new Post(post.getTitolo(), post.getTesto(), post.getAutore()));
-		return _post;
+		return postService.crateNewPost(post);
 	}
 
 	@DeleteMapping("/posts/{id}")
 	public ResponseEntity<String> deletePost(@PathVariable("id") long id) {
-		System.out.println("Delete Post with ID = " + id + "...");
-
-		repository.deleteById(id);
-
-		return new ResponseEntity<>("Post has been deleted!", HttpStatus.OK);
+		return postService.deletePost(id);
 	}
 
 	@DeleteMapping("/posts/delete")
 	public ResponseEntity<String> deleteAllPosts() {
-		System.out.println("Delete All Posts...");
-
-		repository.deleteAll();
-
-		return new ResponseEntity<>("All posts have been deleted!", HttpStatus.OK);
+		return postService.deleteAllPosts();
 	}
 
 	@GetMapping(value = "posts/titolo/{titolo}")
 	public List<Post> findByTitolo(@PathVariable String titolo) {
-
-		List<Post> posts = repository.findByTitolo(titolo);
-		return posts;
+		return postService.findPostByTitolo(titolo);
 	}
 
 	@PutMapping("/posts/{id}")
 	public ResponseEntity<Post> updatePost(@PathVariable("id") long id, @RequestBody Post post) {
-		System.out.println("Update Post with ID = " + id + "...");
-
-		Optional<Post> postData = repository.findById(id);
-
-		if (postData.isPresent()) {
-			Post _post = postData.get();
-			_post.setTitolo(post.getTitolo());
-			_post.setTesto(post.getTesto());
-			_post.setLike(post.getLike());
-			_post.setDislike(post.getDislike());
-			return new ResponseEntity<>(repository.save(_post), HttpStatus.OK);
-		} else {
-			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		}
+		return postService.updatePost(id, post);
 	}
 }
--- backendApp/post-service/src/main/java/com/javasampleapproach/springrest/postgresql/service/PostService.java
+++ backendApp/post-service/src/main/java/com/javasampleapproach/springrest/postgresql/service/PostService.java
@@ -1,4 +1,76 @@
 package com.javasampleapproach.springrest.postgresql.service;
 
+import com.javasampleapproach.springrest.postgresql.model.Post;
+import com.javasampleapproach.springrest.postgresql.repo.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class PostService {
+
+   private final PostRepository postRepository;
+
+   @Autowired
+   public PostService(PostRepository postRepository) {
+      this.postRepository = postRepository;
+   }
+
+   public List<Post> getAllPosts() {
+      System.out.println("Get all Posts...");
+      List<Post> posts = new ArrayList<>();
+      postRepository.findAll().forEach(posts::add);
+
+      return posts;
+   }
+
+   public Post crateNewPost(Post post) {
+      Post _post = postRepository.save(new Post(post.getTitolo(), post.getTesto(), post.getAutore()));
+
+      return _post;
+   }
+
+   public ResponseEntity<String> deletePost( long id) {
+      System.out.println("Delete Post with ID = " + id + "...");
+
+      postRepository.deleteById(id);
+
+      return new ResponseEntity<>("Post has been deleted!", HttpStatus.OK);
+   }
+
+   public ResponseEntity<String> deleteAllPosts() {
+      System.out.println("Delete All Posts...");
+
+      postRepository.deleteAll();
+
+      return new ResponseEntity<>("All posts have been deleted!", HttpStatus.OK);
+   }
+
+   public List<Post> findPostByTitolo(String titolo) {
+      List<Post> posts = postRepository.findByTitolo(titolo);
+
+      return posts;
+   }
+
+   public ResponseEntity<Post> updatePost(long id, Post post) {
+      System.out.println("Update Post with ID = " + id + "...");
+
+      Optional<Post> postData = postRepository.findById(id);
+
+      if (postData.isPresent()) {
+         Post _post = postData.get();
+         _post.setTitolo(post.getTitolo());
+         _post.setTesto(post.getTesto());
+         _post.setLike(post.getLike());
+         _post.setDislike(post.getDislike());
+         return new ResponseEntity<>(postRepository.save(_post), HttpStatus.OK);
+      } else {
+         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+      }
+   }
 }
