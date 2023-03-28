2d833c3e1917b4cf1f81ce89838e70c0bbbe2de5
Andrea  Balbo Mossetto <andreabalbomossetto@MBP-di-Andrea-2.homenet.telecomitalia.it>
2023-03-28 18:51:00 +0200

    aggiunta base per registrazione

---
+++ forum-proj/src/app/sign-in/sign-in.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-sign-in',
+  templateUrl: './sign-in.component.html',
+  styleUrls: ['./sign-in.component.css']
+})
+
+export class SignInComponent {
+  nome: string = '';
+  cognome: string = '';
+  username: string = '';
+  email: string = '';
+  password: string = '';
+  confirmPassword: string = '';
+
+
+  constructor () {}
+
+  onSubmit() {
+    if (this.password !== this.confirmPassword) {
+      alert('Passwords do not match');
+      return;
+    }
+
+    const data = {
+      nome: this.nome,
+      cognome: this.cognome,
+      username: this.username,
+      email: this.email,
+      password: this.password
+    };
+    console.log(data);
+  }
+
+    }
+
